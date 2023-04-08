5a24f116b554243443d6976148f89e9bce08751d
sakib4400 <sakibshahriaremon@gmail.com>
2023-04-08 14:30:40 +0600

    UPGREADED

--- 1045.cpp
+++ 1045.cpp
@@ -1,34 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    double a,b,c;
-
-    scanf("%lf%lf%lf",&a,&b,&c);
-    if(a>=b+c || b>=a+c || c>=c+a)
+    double a, b, c;
+printf("ENTER NUMBER = ");
+    scanf("%lf%lf%lf", &a, &b, &c);
+    if (a >= b + c || b >= a + c || c >= c + a)
     {
         printf("NAO FORMA TRIANGULO\n");
     }
-    else if((a*a)==(b*b)+(c*c) || (b*b)==(a*a)+(c*c) || (c*c)==(b*b)+(a*a))
+    else if ((a * a) == (b * b) + (c * c) || (b * b) == (a * a) + (c * c) || (c * c) == (b * b) + (a * a))
     {
         printf("TRIANGULO RETANGULO\n");
     }
-    else if((a*a)>(b*b)+(c*c)|| (b*b)>(a*a)+(c*c) || (c*c)>(b*b)+(a*a))
+    else if ((a * a) > (b * b) + (c * c) || (b * b) > (a * a) + (c * c) || (c * c) > (b * b) + (a * a))
     {
         printf("TRIANGULO OBTUSANGULO\n");
     }
-    else if((a*a)<(b*b)+(c*c) || (b*b)<(a*a)+(c*c) || (c*c)<(b*b)+(a*a))
+    else if ((a * a) < (b * b) + (c * c) || (b * b) < (a * a) + (c * c) || (c * c) < (b * b) + (a * a))
     {
         printf("TRIANGULO ACUTANGULO\n");
     }
-    if(a==b && a==c)
+    if (a == b && a == c)
     {
         printf("TRIANGULO EQUILATERO\n");
     }
-   if((a == b && a != c)  || (a == c && a != b) || (b == c && b != a))
+    if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
     {
         printf("TRIANGULO ISOSCELES\n");
     }
 
-
-  return 0;
+    return 0;
 }
